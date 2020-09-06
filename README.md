40f8ac47b8d7ac734ebc46ed00a4f127d4b4278b
Shubhro Shekhar <shubhro@symboinsurance.com>
2020-09-06 19:23:20 +0530

    new logging functions added. readme updated. version updated

--- README.md
+++ README.md
@@ -6,13 +6,69 @@ An easy environment based console logging tool.
 
 # How to Use
 
+## set-up env
+#### Default logging 
+if environment variable __NODE_ENV__ is one of _['PROD', 'prod', 'PRODUCTION', 'production']_ logging will be disabled.
+
+### Set custom log mode
+You can override this by setting  `LOG_MODE = TRUE / FALSE`
+
+## Logging
+
+#### normal Logging
+
+```
+... 
+import { consoleLog } from 'dim-logs'; // import
+...
+.
+...
+consoleLog('MY-LOGS'); // logging
+...
 ```
 
-import { consoleLog } from 'dim-logs';
+#### styled Logging 😎
+
+``` 
+import { dimLog } from 'dim-logs';
+.
+.
+.
+dimLog('dim-log is easy', {
+  color: 'green',
+  background: 'skyblue',
+  fontWeight: 'bold',
+  fontSize: 50,
+});
+
+or //
+
+dimLog('dim-log is easy', 'color: green; font-weight: bold;');
+...
+```
+
+#### other console Logging methods
+
+```
+
+import { 
+  consoleLog,
+  consoleError,
+  consoleDebug,
+  consoleAssert,
+  consoleDir,
+  consoleDirxml,
+  dimLog, 
+} from 'dim-logs';
 
 ...
 ...
-consoleLog('Hello World');
+consoleLog('MY-LOGS');
+consoleError('MY-ERROR');
+consoleDebug('MY-DEBUG');
+consoleAssert(false, 'MY-ASSERT');
+consoleDir({ name: 'Navin', address: { city: 'MUMBAI', state: 'maharashtra' } });
+consoleDirxml(document);
 ...
 ...
 
--- index.js
+++ index.js
@@ -1,5 +1,100 @@
-const consoleLog = (...params) => {
-  console.log(...params);
+/* eslint-disable no-console */
+
+const runMode = process.env.NODE_ENV;
+const logMode = process.env.LOG_MODE;
+let logging = true;
+
+const prodRunModes = ['PROD', 'prod', 'PRODUCTION', 'production'];
+const noLoggingModes = ['FALSE', 'false', 'NO', 'no', '0'];
+const loggingModes = ['TRUE', 'true', 'YES', 'yes', '1'];
+
+
+if (prodRunModes.indexOf(runMode) >= 0) {
+  logging = false;
 }
 
-module.exports.consoleLog = consoleLog;
+if (logMode) {
+  if (noLoggingModes.indexOf(logMode) >= 0){
+    logging = false;
+  } else if (loggingModes.indexOf(logMode) >= 0) {
+    logging = true;
+  }
+}
+
+const consoleLog = (...params) => {
+  if (logging) {
+    console.log(...params);
+  }
+};
+
+const consoleError = (...params) => {
+  if (logging) {
+    console.error(...params);
+  }
+};
+
+const consoleDebug = (...params) => {
+  if (logging) {
+    console.debug(...params);
+  }
+};
+
+const consoleAssert = (...params) => {
+  if (logging) {
+    console.assert(...params);
+  }
+};
+
+const consoleDir = (...params) => {
+  if (logging) {
+    console.dir(...params);
+  }
+};
+
+
+const consoleDirxml = (...params) => {
+  if (logging) {
+    console.dirxml(...params);
+  }
+};
+
+const dimLog = (logMessage, style) => {
+  if (logging) {
+    let formatting = '';
+    if (typeof style === 'string') {
+      formatting = style;
+    } else if (typeof style === 'object') {
+      if (style.color) {
+        formatting = `color: ${style.color}; `;
+      }
+      if (style.fontWeight) {
+        formatting += `font-weight: ${style.fontWeight}; `;
+      }
+      if (style.fontSize) {
+        let formattedForntSize = style.fontSize;
+        if (typeof formattedForntSize === 'number') {
+          formattedForntSize = `${formattedForntSize}px`;
+        }
+        formatting += `font-size: ${formattedForntSize}; `;
+      }
+      if (style.background) {
+        formatting += `background: ${style.background}; `;
+      }
+      if (style.fontStyle) {
+        formatting += `font-style: ${style.fontStyle}; `;
+      }
+    }
+    console.log(`%c ${logMessage}`, formatting);
+  }
+};
+
+
+export {
+  consoleLog,
+  consoleError,
+  consoleDebug,
+  consoleAssert,
+  consoleDir,
+  consoleDirxml,
+  dimLog,
+};
